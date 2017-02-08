7c5218c71f6434354d1eac893f90cab8ab4a829f
Atom <fromatom@gmail.com>
2017-02-09 00:37:25 +0900

    Install Stencil

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "sansa",
     dependencies: [
 		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
-		.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4)
-	]
+		.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4),
+		.Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 7),
+  ]
 )
