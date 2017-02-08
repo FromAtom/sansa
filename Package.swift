41a2a1cae31c6b17e0c42b0bc14fabb9e059f313
Atom <fromatom@gmail.com>
2017-02-09 01:26:19 +0900

    Mustacheを使ってHTMLを吐けるようにする (#1)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "sansa",
     dependencies: [
-		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
-		.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4)
+		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+		.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
+		.Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 1, minor: 6)
 	]
 )
